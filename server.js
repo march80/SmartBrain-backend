3ba9500774ba4b0c42ef96ad111b22ce4845bd4a
Matt March <matt.marchcoding@gmail.com>
2023-09-29 12:22:59 -0400

    pushing changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
 
 
 const app = express();
@@ -11,7 +12,6 @@ const database = {
             id: "123",
             name: "John",
             email: "john@gmail.com",
-            password: "cookies",
             entries: 0,
             joined: new Date()
         },
@@ -19,11 +19,17 @@ const database = {
             id: "124",
             name: "Sally",
             email: "sally@gmail.com",
-            password: "bananas",
             entries: 0,
             joined: new Date()
         }
 
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
 
@@ -42,6 +48,9 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
+    bcrypt.hash(password, null, null, function (err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: "125",
         name: name,
@@ -55,13 +64,32 @@ app.post('/register', (req, res) => {
 
     app.get('/profile/:id', (req,res) => {
         const { id } = req.params;
+        let found = false;
         database.users.forEach(user => {
             if(user.id === id){
-                res.json(user);
-            } else {
-                res.status(404).json('no such user');
-            }
+                found = true;
+               return  res.json(user);
+            } 
         });
+        if(!found) {
+            res.status(400).json('not found')
+        }
+    });
+
+    app.put('/image', (req,res) => {
+        const { id } = req.body;
+        let found = false;
+        database.users.forEach(user => {
+            if(user.id === id){
+                found = true;
+                user.entries ++
+               return  res.json(user.entries);
+            } 
+        });
+        if(!found) {
+            res.status(400).json('not found')
+        }
+
     });
 
 
