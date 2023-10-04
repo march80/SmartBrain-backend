cdbcddbdd29e3b3aae9029cd81a0a3a18aff2560
Matt March <matt.marchcoding@gmail.com>
2023-10-03 20:37:58 -0400

    changing back to previous version

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
-    }
-  });
+// const db = knex({
+//   // connect to your own database here:
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'aneagoie',
+//     password : '',
+//     database : 'smart-brain'
+//   }
+// });
   
-  db.select('*').from('users').then(data => {console.log(data)});
+//   db.select('*').from('users').then(data => {console.log(data)});
 
 
 const app = express();
@@ -39,9 +39,7 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)} );
 
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
-app.post('/imageurl', (req,res) => {image.handleAPICall(req,res)});
 
 
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
-})
+
+
