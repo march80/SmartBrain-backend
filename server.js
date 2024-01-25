9698907bc89523d7fe84b147119e56f61334f091
Matt March <matt.marchcoding@gmail.com>
2024-01-25 12:46:46 -0500

    updating server

--- server.js
+++ server.js
@@ -1,45 +1,41 @@
 const express = require('express');
+
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-// const db = knex({
-//   // connect to your own database here:
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     user : 'aneagoie',
-//     password : '',
-//     database : 'smart-brain'
-//   }
-// });
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString : process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
+
+  //db.select('*').from('users').then(data => {console.log(data)});
+
+
+  const app = express();
+
+
+  app.use(cors())
+  app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
   
-//   db.select('*').from('users').then(data => {console.log(data)});
+  app.get('/', (req, res)=> { res.send(db.users) })
+  app.post('/signin', signin.handleSignin(db, bcrypt))
+  app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+  app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+  app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+  app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-
-const app = express();
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use(cors())
-
-
-app.get('/',(req, res) =>{
-    res.send('success')
-});
-
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
-
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
-
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)} );
-
-app.put('/image', (req, res) => {image.handleImage(req, res, db)});
-
-
-
-
+    app.listen(process.env.PORT || 3000, ()=> {
+        console.log(`app is running on port ${process.env.PORT}`)
+    })
